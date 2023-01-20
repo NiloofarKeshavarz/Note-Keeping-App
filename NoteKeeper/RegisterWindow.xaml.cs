59dea6e85347624ce133796274f2b777da12e162
Shiva Elhamian <Shiva.el@gmail.com>
2023-01-20 02:12:36 -0500

    Create RegisterWindow + Implement Its method
    Missed validation and Error handling

---
+++ NoteKeeper/RegisterWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NoteKeeper
+{
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            string username = TbxUsername.Text;
+            string password = TbxPassword.Password;
+            Register(username, password);
+
+        }
+        private void Register(string username, string password)
+        {
+            User newUser = new User();
+            newUser.username = username;
+            newUser.password = password;
+
+            using (var context = new NoteDbContext())
+            {
+                context.Users.Add(newUser);
+            }
+        }
+    }
+}
