367099e7e97f14bc9370e66088c5915ad7316a34
Niloofar Keshavarz <98362008+NiloofarKeshavarz@users.noreply.github.com>
2023-01-21 16:58:15 -0500

    Adds NewNote Handler and edits UI

--- NoteKeeper/MainEditor.xaml.cs
+++ NoteKeeper/MainEditor.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace NoteKeeper
 {
@@ -19,13 +21,30 @@ namespace NoteKeeper
 	/// </summary>
 	public partial class MainEditor : Window
 	{
+
+		List<Note> notes = new List<Note>();
 		public MainEditor()
 		{
 			InitializeComponent();
+			try
+			{
+				Globals.dbContext = new NoteDbContext();
+				lvNote.ItemsSource = Globals.dbContext.Notes.ToList();
+			}
+			catch (SystemException ex)
+			{
+				MessageBox.Show(this, "Connection failed" + ex.Message, "Fatal Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				Environment.Exit(1);
+
+			}
 		}
 
+		// Will clean up the richTextBox
 		private void BtnNewNote_Click(object sender, RoutedEventArgs e)
 		{
+			FlowDocument flowDoc = new FlowDocument(new Paragraph(new Run(""))); // After this constructor is called, the new RichTextBox rtb will contain flowDoc. RichTextBox rtb = new RichTextBox(flowDoc);
+			RtxbNewNote.Document = flowDoc;
+			FlowDocument rtbContents = RtxbNewNote.Document;
 
 		}
 
@@ -33,5 +52,14 @@ namespace NoteKeeper
 		{
 
 		}
+
+		private void lvNote_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
 	}
+
+		
+	
 }
+
